972ad698f73bab72bf77f9f3fe2482d37960919d
Hari <k.hari@capgemini.com>
2022-05-16 15:34:51 +0530

    spring mvc

---
+++ src/main/java/com/demo/SpringMvcStudent/SpringMVcStudentApplication.java
@@ -0,0 +1,13 @@
+package com.demo.SpringMvcStudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMVcStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMVcStudentApplication.class, args);
+	}
+
+}
